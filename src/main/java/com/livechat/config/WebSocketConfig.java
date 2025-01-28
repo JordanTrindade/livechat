20c8a0e70e17e60c6b12430fe0619f161d097fce
JordanTrindade <jordandimst@gmail.com>
2025-01-28 20:59:59 -0300

    Create WebSocketConfig.java

---
+++ src/main/java/com/livechat/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.livechat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //aqui e feita a configuracao do broker
+        //usaremos um broker em memoria
+        registry.enableSimpleBroker("/topics");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/livechat-websocket");
+    }
+}
